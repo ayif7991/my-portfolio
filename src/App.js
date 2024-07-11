1c9a4b6d882978454242c324a604a19182629240
alfiya <alfiya060@gmail.com>
2024-07-12 00:22:02 +0200

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import About from './components/About';
+import Contact from './components/contact';
+import Project from './components/Project';
+import Skills from './components/Skills'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+export default function App(){
+  return(
+    <main className='text-gray-400 bg-gray-900 body-font'>
+    <About/>
+    <Contact/>
+    <Project/>
+    <Skills/>
+    </main>
+
   );
 }
-
-export default App;
